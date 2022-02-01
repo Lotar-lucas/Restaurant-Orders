2759c28691b4f7b5be548ca0ca05072e173737e1
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-31 22:14:10 -0300

    Reader file csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,16 @@
+import csv
+
+
 def analyze_log(path_to_file):
+    data = read(path_to_file)
+    print(data)
     raise NotImplementedError
+
+
+def read(path_to_file):
+    with open(path_to_file) as csv_file:
+        data = csv.DictReader(csv_file, fieldnames=["Nome", "Produto", "Dia"])
+        return list(data)
+
+
+analyze_log("data/orders_1.csv")
