5f9bd8363bda8c4cf00cbcfbf062bb12f54e6ebc
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-02-04 01:42:13 -0300

    test requirement two

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,45 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append(
+            {"name": costumer, "product": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        customer_orders = []
+
+        for element in self.orders:
+            if element["name"] == costumer:
+                customer_orders.append(element["product"])
+
+        return max(customer_orders, key=customer_orders.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_the_products = set()
+        list_orders_never_asked = set()
+
+        for element in self.orders:
+            all_the_products.add(element["product"])
+            if element["name"] == costumer:
+                list_orders_never_asked.add(element["product"])
+
+        return all_the_products - list_orders_never_asked
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        all_days = set()
+        list_days_never_asked = set()
+
+        for element in self.orders:
+            all_days.add(element["day"])
+            if element["name"] == costumer:
+                list_days_never_asked.add(element["day"])
+
+        return all_days - list_days_never_asked
 
     def get_busiest_day(self):
         pass
